79cfbc889f2c0b7c199de81053efa7873cb8a32f
insopitus <jessefor@hotmail.com>
2025-06-16 02:53:23 +0800

    path selection page

---
+++ src/components/SelectDir.vue
@@ -0,0 +1,51 @@
+
+
+<script setup lang="ts">
+import { open } from '@tauri-apps/plugin-dialog';
+import { defineEmits } from 'vue'
+const emit = defineEmits()
+async function chooseDirectory() {
+    open({
+        multiple: false,
+        directory: true,
+    }).then(path => {
+        console.log(path)
+        if(path){
+            emit('path-selected', { path })
+        }
+    }).catch(e => {
+        console.log(e)
+    });
+}
+</script>
+
+<template>
+    <div class="home">
+
+        <div class="mask">
+
+        </div>
+        <button @click="chooseDirectory">Choose a directory...</button>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+button {
+    font-size: 24px;
+    width: 60%;
+    z-index: 1;
+    height: 80px;
+}
+
+.mask {
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 0;
+}
+</style>
